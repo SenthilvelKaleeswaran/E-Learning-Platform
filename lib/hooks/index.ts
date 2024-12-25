9dc79a4f67842504f233c8c37fd17a85bf187b2e
SeenthilvelKaleeswaran <devfallous@gmail.com>
2024-12-25 17:01:13 +0530

    feat(hooks): created hooks for auth module

---
+++ lib/hooks/index.ts
@@ -0,0 +1,9 @@
+import useLogin from "./use-login";
+import useRegister from "./use-register";
+import useLogout from "./use-logout";
+
+export {
+    useLogin,
+    useLogout,
+    useRegister
+}
---
+++ lib/hooks/use-login.ts
@@ -0,0 +1,43 @@
+import { useMutation } from "@tanstack/react-query";
+import { validatePattern } from "@/lib/utils";
+import { emailPattern } from "@/lib/patterns";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+const useLogin = () => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: async (data: any) => {
+      const { email, password } = data;
+      if (!validatePattern(email, emailPattern))
+        return { error: "Provide valid email address !" };
+      const response = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+      return response;
+    },
+    onMutate: () => {
+      toast.loading("We are processing !",{id : "login"});
+    },
+    onSuccess: (data: any) => {
+      console.log({data})
+      if (data?.error) {
+        toast.error(data.error);
+      } else {
+        toast.success("Logged in Successfully !",{id : "login"});
+        router.push("/");
+        router.refresh();
+      }
+    },
+    onError: (error: any) => {
+      console.error("Registration Failed:", error);
+      toast.error(error.message,{id : "login"});
+    },
+  });
+};
+
+export default useLogin;
