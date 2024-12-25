af763446cfca2d19bbf6c0d7b1c09e7267962854
SeenthilvelKaleeswaran <devfallous@gmail.com>
2024-12-25 16:59:16 +0530

    feat(icon): created icon component

---
+++ lib/icon/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import Logo from "./logo";
+
+const iconMap = {
+    Logo
+} as const;
+
+type IconName = keyof typeof iconMap;
+
+interface IconProps {
+  name: IconName;
+  [key: string]: any;
+}
+
+export const Icon: FC<IconProps> = ({ name, ...rest }) => {
+  const IconComponent = iconMap[name];
+
+  if (!IconComponent) {
+    return null;
+  }
+
+  return <IconComponent className="h-4 w-4" {...rest} />;
+};
