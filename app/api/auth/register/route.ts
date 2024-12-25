c4307a09218435bab40d8ded1e3fb6e6ee4fe2b0
SeenthilvelKaleeswaran <devfallous@gmail.com>
2024-12-25 11:23:38 +0530

    feat(api): created api for signup route

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exists.Please login !" },
+        { status: 409 }
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    if (!newUser.id)
+      return NextResponse.json(
+        { error: "Failed to create account. Please try again !" },
+        { status: 500 }
+      );
+
+    return NextResponse.json({ message: "Account created successfully !" });
+    
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Failed to create account. Please try again !" },
+      { status: 500 }
+    );
+  }
+}
