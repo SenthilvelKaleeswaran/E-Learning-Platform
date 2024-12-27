cc9092cf8db015c2b388a414a83563c9db4057ee
SeenthilvelKaleeswaran <devfallous@gmail.com>
2024-12-27 18:15:20 +0530

    fix(my-course) : fixed handle course completion

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -2,6 +2,13 @@ import { validatePattern } from "./validate-pattern";
 import { calculateTotalTopics, formatCourseData } from "./calculate-percentage";
 import { queryParams } from "./query-params";
 import { formatDateTime } from "./format-date-time";
+import { handleCourseCompletion } from "./handle-course-completion";
 
-
-export {calculateTotalTopics, formatCourseData, formatDateTime, queryParams, validatePattern };
+export {
+  calculateTotalTopics,
+  formatCourseData,
+  formatDateTime,
+  handleCourseCompletion,
+  queryParams,
+  validatePattern,
+};
